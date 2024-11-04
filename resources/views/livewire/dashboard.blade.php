08ca666988ce501bd941f0764fe43631798a5df6
Parth Gajjar <parthgajjar@Parths-MacBook-Pro.local>
2024-11-04 17:56:31 +0530

    Improve UI

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,14 +1,41 @@
 <div>
-    <form wire:submit.prevent="request">
-        <div class="flex items-center px-4 py-4">
-            <div class="h-6">
-                <input wire:model="vehicle_image" accept="image/*" class="block text-sm cursor-pointer dark:text-gray-400 focus:outline-none dark:placeholder-gray-400" id="file_input" type="file">
-                    @error('vehicle_image')
-                        <span class="text-sm text-red-600">{{ $message }}</span>
-                    @enderror
+    <form wire:submit.prevent="request" class="p-6">
+        <div
+            class="dropzone-drag flex bg-gray-100 rounded-lg border-solid border-4 border-indigo-400 relative justify-center px-4 py-4 hover:border-dashed">
+            <div class="dropzone-drag-inner text-center">
+
+                <!--   ✅ Here we can put font icon as well   -->
+                <div class="dropzone-drag-icon flex justify-center">
+                    <input wire:model="vehicle_image" accept="image/*" capture="user"
+                        class="absolute inset-0 z-50 m-0 p-0 w-full h-full outline-none opacity-0 cursor-pointer dark:text-gray-400 focus:outline-none dark:placeholder-gray-400"
+                        id="file_input" type="file">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                        class="main-grid-item-icon" fill="none" stroke="currentColor" stroke-linecap="round"
+                        stroke-linejoin="round" stroke-width="2">
+                        <polyline points="16 16 12 12 8 16" />
+                        <line x1="12" x2="12" y1="12" y2="21" />
+                        <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3" />
+                        <polyline points="16 16 12 12 8 16" />
+                    </svg>
+
+                </div>
+
+                <h5 class="dropzone-drag-title py-2">
+                    Open Camera to capture vehicle number
+                </h5>
+
             </div>
 
-            <button class="flex-shrink-0 px-2 py-1 mt-2 text-sm text-white bg-teal-500 border-4 border-teal-500 rounded hover:bg-teal-700 hover:border-teal-700" type="submit">
+            @error('vehicle_image')
+                <span class="text-sm text-red-600">{{ $message }}</span>
+            @enderror
+
+
+        </div>
+        <div class="flex justify-center py-6">
+            <button
+                class="flex-shrink-0 px-2 py-1 mt-2 text-sm text-white bg-teal-500 border-4 border-teal-500 rounded hover:bg-teal-700 hover:border-teal-700"
+                type="submit">
                 Request
             </button>
         </div>
